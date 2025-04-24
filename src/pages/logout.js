9e7fb8c10f41d4aa045db9479af942b6d76ce70c
Lisa-Ann B <lisabruney@pitt.edu>
2025-04-24 13:00:56 -0400

    Delete redirect cookie only on request to logout - #2002

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 import {useContext, useEffect} from 'react'
 import AppContext from '@/context/AppContext'
 import {getLogoutURL} from '@/config/config'
+import {deleteCookie} from "cookies-next";
 
 const Spinner = dynamic(() => import("@/components/custom/Spinner"))
 
@@ -9,6 +10,7 @@ function logout() {
     const {logout} = useContext(AppContext)
     useEffect(() => {
         logout()
+        deleteCookie('redirectUri')
         window.location = getLogoutURL()
     })
     return <Spinner text="Logging you out, please wait... "/>
