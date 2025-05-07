992083bd58a3a8a2618db9d1d255ea4e980751f0
maxsibilla <maxsibilla@icloud.com>
2025-05-07 10:21:12 -0400

    Adding google console meta tag

--- src/components/custom/layout/Header.js
+++ src/components/custom/layout/Header.js
@@ -7,6 +7,7 @@ const Header = ({title}) => {
             <title>{title}</title>
             <link rel="icon" href="/favicon.ico"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <meta name="google-site-verification" content="m72fbKEuY_H6kYypUVFuYcUl6fnapQvPVFUIMRw5NhU" />
         </Head>
     )
 }
