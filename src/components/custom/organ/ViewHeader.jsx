a340515ab81a9c5291f9f22b622570e86a2e6680
Lisa-Ann B <lisabruney@pitt.edu>
2025-10-13 11:59:26 -0400

    Add links for other codes - #2276

--- src/components/custom/organ/ViewHeader.jsx
+++ src/components/custom/organ/ViewHeader.jsx
@@ -7,30 +7,53 @@
  * @returns {JSX.Element} The JSX code for the OrganViewHeader component.
  */
 const OrganViewHeader = ({ organ }) => {
-    return (
-        <div style={{ width: '100%' }}>
-            {/* Title */}
-            <h4>Organ</h4>
-            <h3>{organ.label}</h3>
-
-            {/* Badges */}
-            <div className='row mb-2' style={{ minHeight: '38px' }}>
-                <div className='col-md-6 col-sm-12 entity-subtitle icon-inline'>
-                    <h5 className='title-badge'>
+    const labelLink = (o, colSize = '2') => {
+        return (<div key={o.subLabel} className={`col-md-${colSize} col-sm-12 entity-subtitle icon-inline`}>
+                    <h3 className='me-2'>{o.label}</h3>
+                    <span className='fs-5 title-badge'>
                         <span className='badge bg-secondary'>
                             <a
-                                href={organ.url}
+                                href={o.url}
                                 className='icon-inline text-white'
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
-                                {organ.subLabel}
+                                {o.subLabel}
                             </a>
                             &nbsp;
                             <i className='bi bi-box-arrow-up-right lnk--white'></i>
                         </span>
-                    </h5>
-                </div>
+                    </span>
+                </div>)
+    }
+
+    const formatUrl = (o) => {
+        if (o.includes('UBERON')) {
+            return `http://purl.obolibrary.org/obo/${o.replace(':', '_')}`
+        } else {
+            return `http://purl.org/sig/ont/fma/${o.replace(':', '')}`.toLowerCase()
+        }
+    }
+    const getList = () => {
+        let res = []
+        if (organ.meta) {
+            for (let o in organ.meta) {
+                res.push(labelLink({...organ.meta[o], subLabel: o, url: formatUrl(o)}))
+            }
+        }
+        return res 
+    }
+
+    return (
+        <div style={{ width: '100%' }}>
+            {/* Title */}
+            <h4>Organ</h4>
+            
+
+            {/* Badges */}
+            <div className='row mb-2' style={{ minHeight: '38px' }}>
+                {labelLink(organ, '3')}
+                {getList()}
             </div>
         </div>
     )
