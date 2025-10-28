7a0491f9d89a729436ebefd267958820bc7ddac0
Tyler Madonna <tjm159@pitt.edu>
2025-10-28 13:10:30 -0400

    Adding turbopack support

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -3,10 +3,11 @@ import { resolve } from 'path'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, // Don't render components twice
-    webpack: (config, options) => {
-        // Set the @ alias for the src directory
-        config.resolve.alias['@'] = resolve()
-        return config
+    turbopack: {
+        root: resolve('..'),
+        resolveAlias: {
+            "@": resolve('./src')
+        }
     }
 }
 
