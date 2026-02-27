6fca25fbe335126cdfac0f3fa12d6c577ad8db7b
Lisa-Ann B <lisabruney@pitt.edu>
2026-02-27 09:59:39 -0500

    Update build

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -5,7 +5,6 @@ import pkg from './package.json' with {type: 'json'};
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, // Don't render components twice
-   
     env: {
         // Exposes the version of a specific devDependency (e.g., 'tailwindcss')
         NEXT_PUBLIC_VITESSCE_VERSION: pkg['devDependencies']['vitessce']
