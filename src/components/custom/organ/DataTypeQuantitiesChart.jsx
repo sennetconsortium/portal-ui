72248987485f841818d4bc6dd19f68c21a552c70
Lisa-Ann B <lisabruney@pitt.edu>
2026-03-26 14:41:39 -0400

    Add DataTypeQuantitiesChart - #2534

---
+++ src/components/custom/organ/DataTypeQuantitiesChart.jsx
@@ -0,0 +1,89 @@
+import { VisualizationsProvider } from '@/context/VisualizationsContext'
+import React, {useContext, useRef} from 'react'
+import useSearchUIQuery from '@/hooks/useSearchUIQuery'
+import { getEntitiesIndex } from '@/config/config'
+import { SEARCH_ENTITIES } from '@/config/search/entities'
+import StackedBar from '../visualizations/charts/StackedBar'
+import AppContext from '@/context/AppContext'
+import { Skeleton } from '@mui/material'
+
+function DataTypeQuantitiesChart({ organ }) {
+
+  const mustNot = SEARCH_ENTITIES.searchQuery.excludeFilters.map((filter) => {
+    switch (filter.type) {
+      case 'term':
+        return { terms: { [filter.field]: filter.values } };
+      case 'exists':
+        return { exists: { field: filter.field } };
+    }
+  })
+
+  const query = {
+    size: 0,
+    query: {
+      bool: {
+        filter: {
+          terms: {
+            'origin_samples.organ.keyword': organ.codes,
+          }
+        },
+        must: {
+          term: {
+            "entity_type.keyword": "Dataset"
+          }
+        },
+        must_not: mustNot
+      }
+    },
+    aggs: {
+      by_dataset_type: {
+        terms: {
+          field: "dataset_type_hierarchy.first_level.keyword",
+          size: 1000
+        },
+        aggs: {
+          by_organ_code: {
+            "terms": {
+              "field": "origin_samples.organ.keyword",
+              "size": 100
+            },
+          },
+        }
+      }
+    }
+  }
+
+  const { data, loading, error } = useSearchUIQuery(getEntitiesIndex(), query)
+  const {cache} = useContext(AppContext)
+
+  const subGroupLabels = useRef({}) 
+
+  const getBarData = () => {
+    const _data = []
+    let maxY = 0
+    let organCodes, count
+    for (const d of data.aggregations.by_dataset_type.buckets) {
+      organCodes = {}
+      count = 0
+      for(const o of d.by_organ_code.buckets) {
+        organCodes[o.key] = o.doc_count
+        count += o.doc_count
+        subGroupLabels.current[o.key] = cache.organTypes[o.key]
+      }
+      _data.push({...organCodes, group: d.key, total: d.doc_count})
+      maxY = Math.max(count, maxY)
+    }
+    return { data: _data, ticks: Math.min(10, maxY) }
+  }
+  const barData = data ? getBarData() : null
+  if (!barData || loading) {
+    return <Skeleton variant='rounded' />
+  }
+  return (
+    <div><VisualizationsProvider>
+      <StackedBar data={barData?.data} subGroupLabels={subGroupLabels.current} style={{className: 'mp-3'}} xAxis={{ label: 'Dataset Type'}} yAxis={{ label: 'Count', ticks: barData?.ticks  }} />
+    </VisualizationsProvider></div>
+  )
+}
+
+export default DataTypeQuantitiesChart
