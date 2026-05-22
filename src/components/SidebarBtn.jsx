fa40aaed801437d2fdf846c852d338ad8c553cfc
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-05-22 15:01:31 -0400

    Add sidebar toggle to facets on search - #2573

--- src/components/SidebarBtn.jsx
+++ src/components/SidebarBtn.jsx
@@ -1,14 +1,29 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import AppContext from "../context/AppContext";
 
-function SidebarBtn() {
+function SidebarBtn({target='#sidebar', onClick, initialClass = ''}) {
     const {sidebarVisible, handleSidebar} = useContext(AppContext)
+
+    const handleOnClick = (e) => {
+        e.preventDefault()
+        if (onClick) {
+            onClick(e)
+        } else {
+            handleSidebar(e)
+        }
+    }
+
+    useEffect(() => {
+        $(target).addClass(`collapse collapse-horizontal ${initialClass}`)
+    }, [target, initialClass])
+
+    const isOpen = sidebarVisible || initialClass === 'show'
 
     return (
         <div className="d-none d-md-block sticky-top" id="sections-button">
-            <a href="#" onClick={handleSidebar} data-bs-target="#sidebar" data-bs-toggle="collapse" title={'Toggle menu sidebar'}
-               className={`btn sidebar-drawer-btn ${sidebarVisible ? 'is-open' : ''} icon-inline mb-2`}>
-                {!sidebarVisible && <i className="bi bi-chevron-right"></i>} {sidebarVisible && <i className="bi bi-chevron-left"></i>}</a>
+            <span onClick={(e) => handleOnClick(e)} data-bs-target={target} aria-controls={target.substring(1)} data-bs-toggle="collapse" title={'Toggle menu sidebar'}
+               className={`btn sidebar-drawer-btn ${isOpen ? 'is-open' : ''} icon-inline mb-2`}>
+                {!isOpen && <i className="bi bi-chevron-right"></i>} {isOpen && <i className="bi bi-chevron-left"></i>}</span>
         </div>
     )
 }
