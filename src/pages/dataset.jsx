5735283986b82bd9f5f0d46279c41ddd81b1a83b
Tyler Madonna <tjm159@pitt.edu>
2026-08-19 16:03:28 -0400

    Only create json ld if dataset is primary

--- src/pages/dataset.jsx
+++ src/pages/dataset.jsx
@@ -213,7 +213,7 @@ function ViewDataset() {
                     <Header
                         title={`${data.sennet_id || ''} | Dataset | SenNet`}
                         description={data.description || undefined}
-                        jsonLD={citationData && data.status === "Published" ? getJsonLDMetaData(data, citationData) : undefined}
+                        jsonLD={citationData && data.status === "Published" && datasetIs.primary(data.creation_action) ? getJsonLDMetaData(data, citationData) : undefined}
                         canonical={getCanonicalUrl(data)}
                     />
                 }
