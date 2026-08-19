f6b5b9d3e601cd1482b4a733b2e8dc28ce18f0e8
Tyler Madonna <tjm159@pitt.edu>
2026-08-19 12:02:21 -0400

    Adding ds metadata head tags for google indexing

--- src/components/custom/layout/Header.js
+++ src/components/custom/layout/Header.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Head from 'next/head'
 
-const Header = ({title}) => {
+const Header = ({title, description, jsonLD, canonical}) => {
     return (
         <Head>
             <title>{title}</title>
             <link rel="icon" href="/favicon.ico"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="google-site-verification" content="m72fbKEuY_H6kYypUVFuYcUl6fnapQvPVFUIMRw5NhU" />
+            {description && <meta name="description" content={description} />}
+            {canonical && <link rel="canonical" href={canonical} />}
+            {jsonLD && <script type="application/ld+json">{JSON.stringify(jsonLD)}</script>}
         </Head>
     )
 }
