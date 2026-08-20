c9621f6cc2c46dcc0fa65aa8a869e7d8cbc11ec0
Tyler Madonna <tjm159@pitt.edu>
2026-08-20 10:52:36 -0400

    Making sitemap page generic

--- src/pages/sitemap.xml.jsx
+++ src/pages/sitemap.xml.jsx
@@ -10,7 +10,11 @@ export const getServerSideProps = async ({ res }) => {
     const xml = `
     <?xml version="1.0" encoding="UTF-8"?>
     <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <sitemap><loc>${baseUrl}/sitemap-dataset.xml</loc></sitemap>
+        <sitemap><loc>${baseUrl}/sitemap/dataset.xml</loc></sitemap>
+        <sitemap><loc>${baseUrl}/sitemap/sample.xml</loc></sitemap>
+        <sitemap><loc>${baseUrl}/sitemap/source.xml</loc></sitemap>
+        <sitemap><loc>${baseUrl}/sitemap/collection.xml</loc></sitemap>
+        <sitemap><loc>${baseUrl}/sitemap/publication.xml</loc></sitemap>
     </sitemapindex>
     `.trim()
 
